06fcf5aacfedc5b0435c0f831c6d5ae30280ef43
dotun-prism <dotun.olaniyan@prism.training>
2025-12-06 04:01:11 +0000

    first lesson

---
+++ b42/struct.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Student:
+// - name (string)
+// - id (string)
+// - age (int)
+// - score (int)
+
+
+// struct Student
+// {
+//     char name[50];
+//     char id[10];
+//     int age;
+//     int score[5];
+// };
+
+int main (void)
+{
+    // struct Student dotun;
+    
+    // dotun.age = 30;
+
+    // printf("age: %d", dotun.age);
+
+    typedef int num;
+    num r;
+    r = 12;
+    printf("%d\n", r);
+    return 0;
+}
